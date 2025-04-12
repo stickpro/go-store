623f22f2ac0f4b458c17acd2afcc8efd136dfb7a
Vladislav Bulgakov <stick.qwe@gmail.com>
2025-04-12 18:26:12 +0300

    new base pagination function

---
+++ internal/storage/repository/repository_attributes/custom.go
@@ -0,0 +1,34 @@
+package repository_attributes
+
+import (
+	"context"
+	"github.com/jackc/pgx/v5"
+	"github.com/stickpro/go-store/internal/storage/base"
+)
+
+type ICustomQueries interface {
+	Querier
+	GetWithPaginate(
+		ctx context.Context,
+		params AttributesWithPaginationParams,
+	) (*base.FindResponseWithFullPagination[*FindRow], error)
+}
+
+type CustomQueries struct {
+	*Queries
+	psql DBTX
+}
+
+func NewCustom(psql DBTX) *CustomQueries {
+	return &CustomQueries{
+		Queries: New(psql),
+		psql:    psql,
+	}
+}
+
+func (s *CustomQueries) WithTx(tx pgx.Tx) *CustomQueries {
+	return &CustomQueries{
+		Queries: New(tx),
+		psql:    tx,
+	}
+}
